b7f8bf76504f2025407b55810cb827883c7acf5c
mickeyvip <mickey.vip@gmail.com>
2020-05-29 16:11:04 +0300

    added zoom icons and functionality the display shows operands 1, 2 and operator

--- src/Calculator.elm
+++ src/Calculator.elm
@@ -2,8 +2,10 @@ module Calculator exposing (..)
 
 import Browser
 import Html exposing (Html, div, table, tbody, td, text, th, thead, tr)
-import Html.Attributes exposing (class, colspan, rowspan)
+import Html.Attributes exposing (class, colspan, style)
 import Html.Events exposing (onClick)
+import Svg exposing (svg, use)
+import Svg.Attributes exposing (xlinkHref)
 
 
 type Operator
@@ -23,12 +25,15 @@ type Msg
     | ClickAction Action
     | ClickOperator Operator
     | ClickEquals
+    | ZoomIn
+    | ZoomOut
 
 
 type alias Model =
     { operand1 : Float
     , operand2 : Maybe Float
     , operator : Maybe Operator
+    , zoomFactor : Float
     }
 
 
@@ -37,6 +42,7 @@ initialModel =
     { operand1 = 0.0
     , operand2 = Nothing
     , operator = Nothing
+    , zoomFactor = 1.0
     }
 
 
@@ -108,6 +114,12 @@ update msg model =
                 Nothing ->
                     model
 
+        ZoomIn ->
+            { model | zoomFactor = model.zoomFactor + 0.1 }
+
+        ZoomOut ->
+            { model | zoomFactor = model.zoomFactor - 0.1 }
+
 
 operatorDivide =
     '÷'
@@ -137,6 +149,23 @@ actionDelete =
     '⌫'
 
 
+operatorToString : Operator -> String
+operatorToString operator =
+    String.fromChar <|
+        case operator of
+            Plus ->
+                operatorPlus
+
+            Minus ->
+                operatorMinus
+
+            Multiply ->
+                operatorMultiply
+
+            Divide ->
+                operatorDivide
+
+
 charToHtml : Char -> Html msg
 charToHtml operatorChar =
     operatorChar
@@ -147,49 +176,58 @@ charToHtml operatorChar =
 view : Model -> Html Msg
 view model =
     let
-        displayOperand =
-            if model.operator == Nothing then
-                model.operand1
+        operand1String =
+            String.fromFloat model.operand1
 
-            else
-                case model.operand2 of
-                    Nothing ->
-                        model.operand1
+        operatorString =
+            model.operator
+                |> Maybe.map operatorToString
+                |> Maybe.withDefault ""
 
-                    Just op2 ->
-                        op2
+        operand2String =
+            model.operand2
+                |> Maybe.map String.fromFloat
+                |> Maybe.withDefault ""
     in
-    div [ class "calculator" ]
+    div [ class "calculator", style "zoom" (String.fromFloat model.zoomFactor) ]
         [ table []
             [ thead []
                 [ tr []
                     [ th [ colspan 5, class "display" ]
-                        [ text (String.fromFloat displayOperand) ]
+                        [ text <| operand1String ++ operatorString ++ operand2String ]
                     ]
                 ]
             , tbody []
                 [ tr []
-                    [ td [ rowspan 2, class "delete", onClick (ClickAction Clear) ] [ charToHtml actionClear ]
+                    [ td [ class "delete", onClick (ClickAction Clear) ] [ charToHtml actionClear ]
                     , td [ onClick (ClickDigit 7) ] [ text "7" ]
                     , td [ onClick (ClickDigit 8) ] [ text "8" ]
                     , td [ onClick (ClickDigit 9) ] [ text "9" ]
                     , td [ class "operator", onClick (ClickOperator Divide) ] [ charToHtml operatorDivide ]
                     ]
                 , tr []
-                    [ td [ onClick (ClickDigit 4) ] [ text "4" ]
+                    [ td [ onClick ZoomIn ]
+                        [ svg []
+                            [ use [ xlinkHref "public/icons.svg#icon-zoomin" ] [] ]
+                        ]
+                    , td [ onClick (ClickDigit 4) ] [ text "4" ]
                     , td [ onClick (ClickDigit 5) ] [ text "5" ]
                     , td [ onClick (ClickDigit 6) ] [ text "6" ]
                     , td [ class "operator", onClick (ClickOperator Multiply) ] [ charToHtml operatorMultiply ]
                     ]
                 , tr []
-                    [ td [ rowspan 2, class "delete", onClick (ClickAction Delete) ] [ charToHtml actionDelete ]
+                    [ td [ onClick ZoomOut ]
+                        [ svg []
+                            [ use [ xlinkHref "public/icons.svg#icon-zoomout" ] [] ]
+                        ]
                     , td [ onClick (ClickDigit 1) ] [ text "1" ]
                     , td [ onClick (ClickDigit 2) ] [ text "2" ]
                     , td [ onClick (ClickDigit 3) ] [ text "3" ]
                     , td [ class "operator", onClick (ClickOperator Minus) ] [ charToHtml operatorMinus ]
                     ]
                 , tr []
-                    [ td [ onClick (ClickDigit 0) ] [ text "0" ]
+                    [ td [ class "delete", onClick (ClickAction Delete) ] [ charToHtml actionDelete ]
+                    , td [ onClick (ClickDigit 0) ] [ text "0" ]
                     , td [] [ text "." ]
                     , td [ class "operator", onClick ClickEquals ] [ charToHtml operatorEquals ]
                     , td [ class "operator", onClick (ClickOperator Plus) ] [ charToHtml operatorPlus ]
